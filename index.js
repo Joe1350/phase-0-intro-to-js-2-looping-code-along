554d310c7c4d9837004ca4578093a4be985225af
Joseph Martin <josephmartin1350@gmail.com>
2021-10-18 17:08:27 -0600

    complete

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-for (let age = 30; age < 40; age++) {
-    console.log(`I\'m ${age} years old. Happy birthday to me!`);
-}
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I\'m ${age} years old. Happy birthday to me!`);
+// }
 
-const gifts = ["teddy bear", "drone", "doll"];
+// const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-    for (let i=0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
-    return gifts;
-}
-wrapGifts(gifts);
+// function wrapGifts(gifts) {
+//     for (let i=0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
 
 const cards = ["Lisa", "Kaitlin", "Jan"];
 
+// function writeCards(cards) {
+//     let array = []
+//     for (let i = 0; i < cards.length; i++) {
+//         console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+//         array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+        
+//     }
+//     return array;
+// }
+// writeCards(cards)
+
+// function countDown(n) {
+//     let i = n;
+//     while (i>=0) {
+//         console.log(i--)
+//     }
+// }
+
+// countDown(10);
+
 function writeCards(cards) {
     let array = []
-    for (let i = 0; i < cards.length; i++) {
-        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
-        array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
-        
+    for(let i = 0; i < cards.length; i++) {
+        array.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
     }
-    return array;
+    return array
 }
-writeCards(cards)
+
+console.log(writeCards(cards))
 
 function countDown(n) {
     let i = n;
-    while (i>=0) {
-        console.log(i--)
+    while (i >= 0) {
+        console.log(i--);
     }
 }
-
-countDown(10);
